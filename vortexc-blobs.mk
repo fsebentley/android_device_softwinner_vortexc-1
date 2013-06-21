e5214085f4c386ec172cc62bf2d0fb010f98741f
Blerfont <blerfont@gmail.com>
2013-06-21 13:56:37 +0200

    Update vortexc-blobs.mk

--- vortexc-blobs.mk
+++ vortexc-blobs.mk
@@ -22,8 +22,7 @@ PRODUCT_COPY_FILES += \
 	device/softwinner/vortexc/prebuilt/bin/mkntfs:system/bin/mkntfs \
 	device/softwinner/vortexc/prebuilt/bin/reboot-recovery.sh:system/bin/reboot-recovery.sh \
 	device/softwinner/vortexc/prebuilt/bin/usb_modeswitch:system/bin/usb_modeswitch \
-	device/softwinner/vortexc/prebuilt/bin/rild:system/bin/rild \
-	device/softwinner/vortexc/prebuilt/bin/memsicd:system/bin/memsicd
+	device/softwinner/vortexc/prebuilt/bin/rild:system/bin/rild
 
 # /system/etc
 PRODUCT_COPY_FILES += \
